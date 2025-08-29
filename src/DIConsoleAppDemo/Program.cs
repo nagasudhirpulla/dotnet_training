ef821b06ce42bb811c1cbc86a7c8222bba1c9662
Nagasudhir <Nagasudhir@SUDHIR>
2025-08-29 16:40:28 +0530

    added DI container in console apps demo

---
+++ src/DIConsoleAppDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+//setup DI container
+var serviceProvider = new ServiceCollection()
+    .AddSingleton<IMyService, MyService>()
+    .BuildServiceProvider();
+
+//get the service from DI container and use it
+var srv = serviceProvider.GetService<IMyService>();
+srv?.DoSomething();
+
+Console.WriteLine("Completed!");
+
+public interface IMyService
+{
+    void DoSomething();
+}
+
+public class MyService : IMyService
+{
+    public void DoSomething()
+    {
+        Console.WriteLine("MyService is doing something.");
+    }
+}
